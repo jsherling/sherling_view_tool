3b1f4c72acb6122d5274bd0dc232f397e5c7c538
jsherling <sherling@xmission.com>
2021-05-27 17:09:16 -0700

    Add Renderer condition, tests

--- lib/sherling_view_tool/renderer.rb
+++ lib/sherling_view_tool/renderer.rb
@@ -2,6 +2,7 @@ module SherlingViewTool
   # Copyright renderer class
   class Renderer
     def self.copyright(name = 'Name not provided!', message = 'All rights reserved')
+      name = 'Name not provided!' if name.empty?
       "&copy; #{Time.now.year} | <b>#{name}</b> #{message}"
     end
   end
--- test/sherling_view_tool_test.rb
+++ test/sherling_view_tool_test.rb
@@ -3,22 +3,37 @@
 require_relative 'test_helper'
 
 class SherlingViewToolTest < Minitest::Test
+  def setup
+    @year = Time.now.year
+    @core = "&copy; #{@year} | "
+  end
+
   def test_tool_version_number_is_correct
     assert_equal '0.2.5', SherlingViewTool::VERSION
   end
 
   def test_no_argument_copyright_notice
     @copyright = SherlingViewTool::Renderer.copyright
-    assert_equal '&copy; 2021 | <b>Name not provided!</b> All rights reserved', @copyright
+    assert_equal "#{@core}<b>Name not provided!</b> All rights reserved", @copyright
   end
 
   def test_one_argument_copyright_notice
     @copyright = SherlingViewTool::Renderer.copyright('a')
-    assert_equal '&copy; 2021 | <b>a</b> All rights reserved', @copyright
+    assert_equal "#{@core}<b>a</b> All rights reserved", @copyright
   end
 
   def test_two_argument_copyright_notice
     @copyright = SherlingViewTool::Renderer.copyright('a', 'b')
-    assert_equal '&copy; 2021 | <b>a</b> b', @copyright
+    assert_equal "#{@core}<b>a</b> b", @copyright
+  end
+
+  def test_allow_empty_message
+    @copyright = SherlingViewTool::Renderer.copyright('a', '')
+    assert_equal "#{@core}<b>a</b> ", @copyright
+  end
+
+  def test_two_empty_arguments
+    @copyright = SherlingViewTool::Renderer.copyright('', '')
+    assert_equal "#{@core}<b>Name not provided!</b> ", @copyright
   end
 end
